f9c1dc80d76733307dfa039df62bad053882ac39
atomiks <sprytex@gmail.com>
2017-03-04 11:25:16 +1100

    Update mean-adjuster.js

--- mean-adjuster.js
+++ mean-adjuster.js
@@ -7,7 +7,7 @@ class MeanAdjuster {
     getTrueMean() {
         return this.values.reduce((sum, value) => sum + value) / this.values.length;
     }
-    
+
     getAdjustedMean() {
         return this.adjustedMean;
     }
@@ -15,7 +15,7 @@ class MeanAdjuster {
     getStandardDeviation() {
         const mean = this.getTrueMean();
         const variability = [];
-        this.values.forEach((rating) => variability.push(Math.pow(rating - mean, 2)));
+        this.values.forEach((value) => variability.push(Math.pow(value - mean, 2)));
         const sum = variability.reduce((sum, value) => sum + value);
         return Math.sqrt(sum / this.values.length);
     }
@@ -24,21 +24,21 @@ class MeanAdjuster {
         const mean = this.getTrueMean();
         const sd = this.getStandardDeviation();
         const valueWeights = [];
-        this.values.forEach(rating => {
-            const zScore = sd !== 0 ? (rating - mean) / sd : 0;
+        this.values.forEach(value => {
+            const zScore = sd !== 0 ? (value - mean) / sd : 0;
             let weight = Math.sin(zScore + Math.PI / 2);
             if (weight < 0) weight = 0;
-            valueWeights.push({rating: rating, weight: weight});
+            valueWeights.push({value: value, weight: weight});
         });
         return valueWeights;
     }
 
     _calculateAdjustedMean() {
-        const ratingWeights = this._applyWeightingFunction();
+        const valueWeights = this._applyWeightingFunction();
         let valueSum = 0;
         let weightSum = 0;
-        ratingWeights.forEach(item => {
-            valueSum += item.rating * item.weight;
+        valueWeights.forEach(item => {
+            valueSum += item.value * item.weight;
             weightSum += item.weight;
         });
         this.adjustedMean = valueSum / weightSum;
