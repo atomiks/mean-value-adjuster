048c5d238bb2b42dbab6dc57a07397db086acb26
atomiks <cc.glows@gmail.com>
2019-01-08 01:26:40 +1100

    Updating for 2019

--- mean-adjuster.js
+++ mean-adjuster.js
@@ -1,50 +1,54 @@
-class MeanAdjuster {
-    constructor(values) {
-        this.values = values;
-        if (!values || values.constructor !== Array || !values.length){
-            console.warn('The class needs an array of numbers. Using default of [0]');
-            this.values = [0];
-        }
-        this._calculateAdjustedMean();
-    }
+// Using `for` loops here instead of functional array methods
+// because they are much faster. This function may be passed
+// thousands or millions of numbers, so it should be as performant
+// as possible.
+//
+// Results in Chrome console for 1,000,000 numbers in `values`:
+// * Functional using Array#map, Array#reduce ≈ 1100ms
+// * Imperative using for loops ≈ 120ms
 
-    getTrueMean() {
-        return this.values.reduce((sum, value) => sum + value) / this.values.length;
-    }
-
-    getAdjustedMean() {
-        return this.adjustedMean;
-    }
-
-    getStandardDeviation() {
-        const mean = this.getTrueMean();
-        const variability = [];
-        this.values.forEach((value) => variability.push(Math.pow(value - mean, 2)));
-        const sum = variability.reduce((sum, value) => sum + value);
-        return Math.sqrt(sum / this.values.length);
-    }
-
-    _applyWeightingFunction()  {
-        const mean = this.getTrueMean();
-        const sd = this.getStandardDeviation();
-        const valueWeights = [];
-        this.values.forEach(value => {
-            const zScore = sd !== 0 ? (value - mean) / sd : 0;
-            let weight = Math.sin(zScore + Math.PI / 2);
-            if (weight < 0) weight = 0;
-            valueWeights.push({value: value, weight: weight});
-        });
-        return valueWeights;
-    }
-
-    _calculateAdjustedMean() {
-        const valueWeights = this._applyWeightingFunction();
-        let valueSum = 0;
-        let weightSum = 0;
-        valueWeights.forEach(item => {
-            valueSum += item.value * item.weight;
-            weightSum += item.weight;
-        });
-        this.adjustedMean = valueSum / weightSum;
-    }
+function getSum(values) {
+  let sum = 0
+  for (let i = 0; i < values.length; i++) {
+    sum += values[i]
+  }
+  return sum
 }
+
+function getMean(values) {
+  return getSum(values) / values.length
+}
+
+function getStandardDeviation(values, mean) {
+  const variability = []
+  for (let i = 0; i < values.length; i++) {
+    variability[i] = (values[i] - mean) ** 2
+  }
+  return Math.sqrt(getSum(variability) / values.length)
+}
+
+function getWeightedValues(values) {
+  const mean = getMean(values)
+  const sd = getStandardDeviation(values, mean)
+  const weightedValues = []
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i]
+    const zScore = sd !== 0 ? (value - mean) / sd : 0
+    const weighting = Math.max(0, Math.sin(zScore + Math.PI / 2))
+    weightedValues[i] = { value, weighting }
+  }
+  return weightedValues
+}
+
+function adjustedMean(values) {
+  const weightedValues = getWeightedValues(values)
+  let sum = 0
+  let weightingSum = 0
+  for (let i = 0; i < weightedValues.length; i++) {
+    sum += weightedValues[i].value * weightedValues[i].weighting
+    weightingSum += weightedValues[i].weighting
+  }
+  return sum / weightingSum
+}
+
+export default adjustedMean
